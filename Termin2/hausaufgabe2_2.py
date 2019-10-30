9c2007f7582ecda4623da6253df1677d28289793
Luka Zdravkovic <luka.z@hotmail.de>
2019-10-30 14:22:26 +0100

    removes tabs in empty lines

--- Termin2/hausaufgabe2_2.py
+++ Termin2/hausaufgabe2_2.py
@@ -6,10 +6,10 @@ from skimage.io import imread, imsave
 
 def eukl_dis(img_1, img_2):
     bin_count = 8
-    
+
     histo_1 = np.histogram(img_1, bins=bin_count, range =(0,256))
     histo_2 = np.histogram(img_2, bins=bin_count, range =(0,256))
-    
+
     summe = 0
     for index in range(bin_count-1):
         diff =  histo_1[0][index-1]-histo_2[0][index-1]

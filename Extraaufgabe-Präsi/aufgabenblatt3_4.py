cc394a683c0a702bdac1c82b2a5078db527c0a01
LukaZdr <luka.z@hotmail.de>
2019-11-05 00:28:02 +0100

    aufgeraeumte version

--- Extraaufgabe-Präsi/aufgabenblatt3_4.py
+++ Extraaufgabe-Präsi/aufgabenblatt3_4.py
@@ -3,41 +3,45 @@
 import numpy as np
 
 # Methoden zum 1d-Farbhistogramme erstellen
-def rgb_img_to_1d_histo(img, bin_count):
+def rgb_img_to_1d_histo(img, bin_count): # nimmt ein bild und macht es zu einem 1d histogramm
     img = img.ravel() # ravel => returns flattend 1d-Array
     return np.histogram(img, bins = bin_count, range=(0,256))
 
-def rgb_imgs_to_1d_histos(img_list, bin_count):
-    return list(map(lambda img: rgb_img_to_1d_histo(img, bin_count), img_list))
-
-def eukl_dist(x,y):
+def eukl_dist(x,y): 
 		return np.sqrt(np.sum((x-y)**2))
 
-def rgb_img_mean(img):
-	return np.mean(np.array(img[:,:,2]))
+def rgb_img_mean(img): # der farb mittelwert eines bildes
+	return np.mean(img[:,:,2])
+	# return np.mean(img)
 
-def indices_of_n_smallest_values(n, num_list):
+def indices_of_n_smallest_values(n, num_list): # gibt die indizes der n kleinsten elemente des arrays zurueck
 	return np.array(num_list).argsort()[:n]
 
-def elements_for_index_list(index_list, elements_list):
+def elements_for_index_list(index_list, elements_list): # gibt fuer eine liste an indizes und eine an elementen die elemente der indizes zurueck
 	return list(map(lambda x: elements_list[x], index_list))
 
-def most_common_occurrence(element_list):
+def most_common_occurrence(element_list): # gibt das am oeften auftretende element zurueck
 	return max(element_list,key=element_list.count)
 
-def calculate_combined_weighted_distanze(image_1, weight, image_2):
+def calculate_combined_weighted_distanze(image_1, weight, image_2): # nimmt die euklidische distanz von dem mittelwert und histogrammen und summiert sie gewichtet auf
 	distance_1 = eukl_dist(rgb_img_mean(image_1),
 												 rgb_img_mean(image_2))
+	#mit 3d histogrammen
+	# image_1 =  image_1.reshape((image_1.shape[0]*image_1.shape[1],3))
+	# image_2 =  image_2.reshape((image_2.shape[0]*image_2.shape[1],3))
+	# distance_2 = eukl_dist(np.histogramdd(image_1, bins = [8,8,8], range=((0,256),(0,256),(0,256)))[0],
+	# 											 np.histogramdd(image_2, bins = [8,8,8], range=((0,256),(0,256),(0,256)))[0])
+	
+	# mit 2d histogrammen										
 	distance_2 = eukl_dist(rgb_img_to_1d_histo(image_1, bin_count)[0],
 												 rgb_img_to_1d_histo(image_2, bin_count)[0])
 	return distance_1 + weight * distance_2
 
-def color_histo_n_nearest_neighbour(va_imgs, tr_imgs, neighbour_count):
+def rgb_img_n_nearest_neighbour(va_imgs, tr_imgs, neighbour_count): # brechnet durch die n naechsten nachbarn das warcheinliche label
 	# guard clause wenn neighborcount hoeher ist als die liste
 	est_labels_list = []
 	for va_img in va_imgs:
 		distances = []
-
 		for tr_img in tr_imgs:
 			distance = calculate_combined_weighted_distanze(va_img, weight, tr_img)
 			distances.append(distance)
@@ -47,31 +51,27 @@ def color_histo_n_nearest_neighbour(va_imgs, tr_imgs, neighbour_count):
 		est_labels_list.append(est_label)
 	return est_labels_list
 
-def guessing_accuracy(est_labels, va_labels):
-	# print("berechnete Klassifizierung mit Mittelwerten:")
-	# print(est_labels)
-	# print("korrekte Werte:")
-	# print(va_labels)
+def guessing_accuracy(est_labels, va_labels): # Wirft auf der console infos aus und retuned genauigkeit in %
 
 	count = 0
 	for ind in range(len(est_labels)):
 			if est_labels[ind] == va_labels[ind]: count += 1
-
-	# print("Treffer:")
-	# print(count)
+	# fuer das checken auf der console 
 	print('////////////////////////////////////')
 	print('bin_count:', bin_count)
 	print('neighbours', count_neighbour)
 	print('weight:', weight)
 	print(count/len(va_imgs)*100, "%")
+
+	# rueckgabe wert = accuracy in %
 	return count/len(va_imgs)*100
 
 # Params
-# Bilder laden und vorbereiten
+bin_count = 6
+count_neighbour = 5
+weight = 0.1
 
-# bin_count = 9
-# count_neighbour = 7
-# weight = 0
+# Bilder laden und vorbereiten
 d = np.load('./trainingsDatenFarbe2.npz')
 tr_imgs = d['data']
 tr_labels = d['labels']
@@ -80,20 +80,12 @@ v = np.load('./validierungsDatenFarbe2.npz')
 va_imgs = v['data']
 va_labels = v['labels']
 
-neighbours = [3, 5, 10]
-ahhhh = []
-for bin_count in range(2,11):
-	print(bin_count)
-	for count_neighbour in range(2,10):
-		for weight in [0]:
+#Ausfuehren der Auswertung
+est_labels = rgb_img_n_nearest_neighbour(va_imgs, tr_imgs, count_neighbour)
 
-			#Ausfuehren der Auswertung
-			est_labels = color_histo_n_nearest_neighbour(va_imgs, tr_imgs, count_neighbour)
+# Statistik
+print(guessing_accuracy(est_labels, va_labels))
 
-			# Statistik
-			ahhhh.append(guessing_accuracy(est_labels, va_labels))
-
-print(max(ahhhh))
 
 # bin_count: 6
 # neighbours 5

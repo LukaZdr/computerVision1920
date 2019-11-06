3b7539e5046da67d9ad1890daf9f582ea646a062
LukaZdr <luka.z@hotmail.de>
2019-11-06 01:11:50 +0100

    wip

--- Extraaufgabe-Präsi/aufgabenblatt3_4.py
+++ Extraaufgabe-Präsi/aufgabenblatt3_4.py
@@ -2,26 +2,30 @@
 
 import numpy as np
 
-# Methoden zum 1d-Farbhistogramme erstellen
-def rgb_img_to_1d_histo(img, bin_count): # nimmt ein bild und macht es zu einem 1d histogramm
+def rgb_img_to_1d_histo(img):
 	img = img.ravel() # ravel => returns flattend 1d-Array
 	return np.histogram(img, bins = bin_count, range=(0,256))
 
-def rgb_img_to_3d_histo(img, bin_count):
+def rgb_img_to_3d_histo(img):
 	img =  img.reshape((img.shape[0]*img.shape[1],3))
 	return np.histogramdd(img, bins = [bin_count,bin_count,bin_count], range=((0,256),(0,256),(0,256)))
 
 def eukl_dist(x,y): 
 		return np.sqrt(np.sum((x-y)**2))
+	
+def intersection(hist_1, hist_2):
+    minima = np.minimum(hist_1, hist_2)
+    intersection = np.true_divide(np.sum(minima), np.sum(hist_2))
+    return intersection
 
-def rgb_img_mean(img): # der farb mittelwert eines bildes
+def rgb_img_mean(img): # der Farbmittelwert eines Bildes
 	return np.mean(img, axis = (0,1,2))
 
-def rgb_img_std(img): # Standart deviation
-	return np.std(img)
-
-def indices_of_n_smallest_values(n, num_list): # gibt die indizes der n kleinsten elemente des arrays zurueck
+def indices_of_n_smallest_values(n, num_list): # gibt die indizes der n kleinsten elemente des arrays zurück
 	return np.array(num_list).argsort()[:n]
+
+def indices_of_n_biggest_values(n, num_list): # gibt die indizes der n größten elemente des arrays zurück
+	return np.array(num_list).argsort()[-n:][::-1]
 
 def elements_for_index_list(index_list, elements_list): # gibt fuer eine liste an indizes und eine an elementen die elemente der indizes zurueck
 	return list(map(lambda x: elements_list[x], index_list))
@@ -30,19 +34,46 @@ def most_common_occurrence(element_list): # gibt das am oeften auftretende eleme
 	return max(element_list,key=element_list.count)
 
 def calculate_combined_weighted_distanze(image_1, weight, image_2): # nimmt die euklidische distanz von dem mittelwert und histogrammen und summiert sie gewichtet auf
-	distance_1 = eukl_dist(rgb_img_std(image_1),
-												 rgb_img_std(image_2))
-	#mit 3d histogrammen
-	# distance_2 = eukl_dist(rgb_img_to_3d_histo(image_1, bin_count)[0],
-	# 											 rgb_img_to_3d_histo(image_2, bin_count)[0])
+	# Beladen der ersten beiden Merkmale
+	if descriptor_1 == '1d_histo':
+		deskr_1_img_1 = rgb_img_to_1d_histo(image_1)
+		deskr_1_img_2 = rgb_img_to_1d_histo(image_2)
+	elif descriptor_1 == '3d_histo':
+		deskr_1_img_1 = rgb_img_to_3d_histo(image_1)
+		deskr_1_img_2 = rgb_img_to_3d_histo(image_2)
+	elif descriptor_1 == 'std':
+		deskr_1_img_1 = np.std(image_1)
+		deskr_1_img_2 = np.std(image_2)
+	elif descriptor_1 == 'mean':
+		deskr_1_img_1 = rgb_img_mean(image_1)
+		deskr_1_img_2 = rgb_img_mean(image_2)
+
+	# Beladen der zweiten beiden Merkmale
+	if descriptor_1 == '1d_histo':
+		deskr_2_img_1 = rgb_img_to_1d_histo(image_1)
+		deskr_2_img_2 = rgb_img_to_1d_histo(image_2)
+	elif descriptor_1 == '3d_histo':
+		deskr_2_img_1 = rgb_img_to_3d_histo(image_1)
+		deskr_2_img_2 = rgb_img_to_3d_histo(image_2)
+	elif descriptor_1 == 'std':
+		deskr_2_img_1 = np.std(image_1)
+		deskr_2_img_2 = np.std(image_2)
+	elif descriptor_1 == 'mean':
+		deskr_2_img_1 = rgb_img_mean(image_1)
+		deskr_2_img_2 = rgb_img_mean(image_2)
+	
+	if dist_ma == 'euklid':
+
+	distance_1 = eukl_dist(deskr_1_img_1, deskr_1_img_2)
+	
+	distance_2 = eukl_dist(rgb_img_to_3d_histo(image_1, bin_count)[0],
+												 rgb_img_to_3d_histo(image_2, bin_count)[0])
 	
 	# mit 2d histogrammen										
-	distance_2 = eukl_dist(rgb_img_to_1d_histo(image_1, bin_count)[0],
-												 rgb_img_to_1d_histo(image_2, bin_count)[0])
-	return weight * distance_1 + distance_2
-	# return distance_2
+	# distance_2 = eukl_dist(rgb_img_to_1d_histo(image_1, bin_count)[0],
+	# 											 rgb_img_to_1d_histo(image_2, bin_count)[0])
+	return distance_1 + weight * distance_2
 	
-
 def rgb_img_n_nearest_neighbour(va_imgs, tr_imgs, neighbour_count): # brechnet durch die n naechsten nachbarn das warcheinliche label
 	est_labels_list = []
 	for va_img in va_imgs:
@@ -57,24 +88,20 @@ def rgb_img_n_nearest_neighbour(va_imgs, tr_imgs, neighbour_count): # brechnet d
 	return est_labels_list
 
 def guessing_accuracy(est_labels, va_labels): # Wirft auf der console infos aus und retuned genauigkeit in %
-
 	count = 0
 	for ind in range(len(est_labels)):
 			if est_labels[ind] == va_labels[ind]: count += 1
-	# fuer das checken auf der console 
-	print('////////////////////////////////////')
-	print('bin_count:', bin_count)
-	print('neighbours', count_neighbour)
-	print('weight:', weight)
-	print(count/len(va_imgs)*100, "%")
-
-	# rueckgabe wert = accuracy in %
 	return count/len(va_imgs)*100
 
 # Params
-# bin_count = 6
-# count_neighbour = 1
-# weight = 0.1
+bin_count = 3
+count_neighbour = 10
+weight = 3
+# distanzmas ist entweder euklid oder intersect
+dist_ma = 'euklid' 
+# diskriptoren sind: 1d_histo, 3d_histo, std, mean
+descriptor_1 = '3d_histo'
+descriptor_2 = 'std'
 
 # Bilder laden und vorbereiten
 d = np.load('./trainingsDatenFarbe2.npz')
@@ -85,19 +112,9 @@ v = np.load('./validierungsDatenFarbe2.npz')
 va_imgs = v['data']
 va_labels = v['labels']
 
-all = []
-for bin_count in range(2,10):
-	for count_neighbour in [3,5,10]:
-		for weight in range(1,11):
-			#Ausfuehren der Auswertung
-			est_labels = rgb_img_n_nearest_neighbour(va_imgs, tr_imgs, count_neighbour)
+#Ausfuehren der Auswertung
+est_labels = rgb_img_n_nearest_neighbour(va_imgs, tr_imgs, count_neighbour)
 
-			# Statistik
-			all.append(guessing_accuracy(est_labels, va_labels))
+# Statistik
+print(guessing_accuracy(est_labels, va_labels))
 
-print(all)
-#bestes ergebniss
-# bin_count: 6
-# neighbours 5
-# weight: 0.1
-# 66.66666666666666 %

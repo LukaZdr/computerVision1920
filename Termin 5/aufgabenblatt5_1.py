695bf0e36205753446e547f3631044b6fb10bc87
Luka Zdravkovic <luka.z@hotmail.de>
2019-11-13 16:56:41 +0100

    4.1.2

--- Termin 5/aufgabenblatt5_1.py
+++ Termin 5/aufgabenblatt5_1.py
@@ -5,25 +5,26 @@ from skimage.io import imread, imshow
 img = imread("./catGNoisy.png")
 plt.imshow(img, cmap="Greys_r")
 
-
 #implement
 def faltung(img):
+  bildMitRahmen = np.zeros((img.shape[0]+2, img.shape[1]+2))
+  bildMitRahmen[1:-1, 1:-1] = img
   soft_img = []
-  for x in range(1,len(img)-1):
+  for x in range(1,len(bildMitRahmen)-1):
     soft_img.append([])
-    for y in range(1,len(img[0])-1):
+    for y in range(1,len(bildMitRahmen[0])-1):
       pix_list = []
-      pix_list.append(img[x-1][y-1])
-      pix_list.append(img[x-1][y])
-      pix_list.append(img[x-1][y+1])
-      pix_list.append(img[x][y-1])
-      pix_list.append(img[x][y])
-      pix_list.append(img[x][y+1])
-      pix_list.append(img[x+1][y-1])
-      pix_list.append(img[x+1][y])
-      pix_list.append(img[x+1][y+1])
+      pix_list.append(bildMitRahmen[x-1][y-1])
+      pix_list.append(bildMitRahmen[x-1][y])
+      pix_list.append(bildMitRahmen[x-1][y+1])
+      pix_list.append(bildMitRahmen[x][y-1])
+      pix_list.append(bildMitRahmen[x][y])
+      pix_list.append(bildMitRahmen[x][y+1])
+      pix_list.append(bildMitRahmen[x+1][y-1])
+      pix_list.append(bildMitRahmen[x+1][y])
+      pix_list.append(bildMitRahmen[x+1][y+1])
       soft_img[x-1].append(np.mean(pix_list))
   return soft_img
-print(np.shape(img))
-print(np.shape(faltung(img)))
+
 plt.imshow(faltung(img), cmap="Greys_r")
+plt.show()
